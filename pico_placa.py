db967d6aa82054ce167770fb1c6d903f5285c051
Tubaher <tubarao0705@gmail.com>
2021-10-19 02:51:22 -0500

    main returning True

--- pico_placa.py
+++ pico_placa.py
@@ -6,10 +6,18 @@ pico_placa,py <plate_number> <date> <time>"""
 
 
 import argparse
+from typing import Mapping
 
 parser = argparse.ArgumentParser(description='Determine if a car should be in the road based on Pico&Placa')
 parser.add_argument('plate_number', type=str, help='full number of the plate')
 parser.add_argument('date', type=str, help='date of the query in format Year-Month-Day')
 parser.add_argument('time', type=str, help='time of the query in format Hour:Minute')
 
-args = parser.parse_args()
+
+
+def main():
+    args = parser.parse_args()
+    print(True)
+
+if __name__ == "__main__":
+    main()
